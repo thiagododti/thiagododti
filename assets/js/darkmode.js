eb72fbd48d3284c3d34f49e7045ee6355efe45b4
thiagododti <t.dod@live.com>
2022-07-28 17:24:57 -0300

    dark mode no css

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,20 +1,38 @@
 function escolherModo() {
     trocarClasse();
+    trocaTexto();
 }
 
 function trocarClasse() {
-    BtnDm.classList.toggle('dark-mode');
-    body.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode');
-    a.classList.toggle('dark-mode');
-    textoApresentacao.classList.toggle('dark-mode');
+    BtnDm.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+    a.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    h2.classList.toggle(darkModeClass);
+    header.classList.toggle(darkModeClass);
 }
 
+function trocaTexto(){
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+    if(BtnDm.classList.contains(darkModeClass)){
+        BtnDm.innerHTML = lightMode;
+        return;
+    }
+
+    BtnDm.innerHTML = darkMode;
+}
+
+const darkModeClass = 'dark-mode';
 const BtnDm = document.getElementById('btn-dm');
-const textoApresentacao = document.getElementsByClassName('texto-apresentacao');
 const body = document.getElementsByTagName('body')[0];
 const footer = document.getElementsByTagName('footer')[0];
 const a = document.getElementsByTagName('a')[0];
+const h1 = document.getElementsByTagName('h1')[0];
+const h2 = document.getElementsByTagName('h2')[0];
+const p = document.getElementsByTagName('p')[0];
+const header = document.getElementsByTagName('header')[0];
 
 
 
